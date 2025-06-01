55dfdb1e90ec780f947183c3f39dab5d0a8a25f6
qmm64 <10gmagma01@gmail.com>
2025-06-01 21:30:46 +0700

    WorkScheduleManager приведён к единому стилю с остальными Manager

---
+++ AppiHHRuInWinForms/Entities/Common/IssuanceCommands/GetWorkDaySchedules.cs
@@ -0,0 +1,33 @@
+﻿using AppiHHRuInWinForms.Entities.Common.Responses;
+using AppiHHRuInWinForms.Entities.Common.Responses.WorkScheduleManagerP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppiHHRuInWinForms.Entities.Common.IssuanceCommands
+{
+    public class GetWorkDaySchedules : IssuanceCommands
+    {
+        public GetWorkDaySchedules(ExtraditionManager extraditionManager)
+        {
+            this.extraditionManager = extraditionManager;
+        }
+
+        public override string Description()
+        {
+            return "По графику работы";
+        }
+
+        public override async Task<List<string>> Execute()
+        {
+            var result = await ((WorkScheduleManager)extraditionManager).GetAllDaySchedulePercent();
+            if (result.IsSuccess)
+            {
+                return result.WorkSchedules;
+            }
+            return null;
+        }
+    }
+}
--- AppiHHRuInWinForms/Entities/Common/Responses/WorkScheduleManagerP/WorkScheduleManager.cs
+++ AppiHHRuInWinForms/Entities/Common/Responses/WorkScheduleManagerP/WorkScheduleManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static AppiHHRuInWinForms.Entities.Common.Responses.WorkScheduleManagerP.WorkScheduleManager;
 
 namespace AppiHHRuInWinForms.Entities.Common.Responses.WorkScheduleManagerP;
 
@@ -13,7 +14,15 @@ internal class WorkScheduleManager : ExtraditionManager
     {
     }
 
-    public async Task<GetAllDaySchedulePercentResponce> GetAllDaySchedulePercent(WorkScheduleFormat workScheduleFormat)
+    private Dictionary<string, string> WorkScheduleFormats = new Dictionary<string, string>{
+        { "FIVE_ON_TWO_OFF","5 через 2"},
+        { "TWO_ON_TWO_OFF","2 через 2"},
+        { "SIX_ON_ONE_OFF","6 через 1"},
+        { "FOUR_ON_THREE_OFF","4 через 3"},
+        { "THREE_ON_TWO_OFF","3 через 2"},
+    };
+
+    public async Task<GetAllDaySchedulePercentResponce> GetAllDaySchedulePercent()
     {
         var responce = await _httpClient.GetAnyVacancies();
         if (!responce.IsSuccess)
@@ -21,9 +30,14 @@ internal class WorkScheduleManager : ExtraditionManager
             Console.WriteLine("Не удалось распарсить вакансии");
             return new GetAllDaySchedulePercentResponce(false);
         }
-        var countAllDaySchefule = responce.Vacancies.Count(vacancy => vacancy.WorkingHoursByDays.Any(e => e.WorkScheduleByDaysId == ConvertWorkScheduleFormatToString(workScheduleFormat)));
-        double allDayPercent = (double)countAllDaySchefule / (double)responce.Vacancies.Count * 100;
-        return new GetAllDaySchedulePercentResponce(true, allDayPercent);
+        List<string> daySchedules = new();
+        foreach(var format in WorkScheduleFormats.Keys)
+        {
+            var countAllDaySchefule = responce.Vacancies.Count(vacancy => vacancy.WorkingHoursByDays.Any(e => e.WorkScheduleByDaysId == format));
+            double allDayPercent = (double)countAllDaySchefule / (double)responce.Vacancies.Count * 100;
+            daySchedules.Add($"График {WorkScheduleFormats[format]} - {countAllDaySchefule}, процент от общего числа: {allDayPercent}");
+        }
+        return new GetAllDaySchedulePercentResponce(true, daySchedules);
     }
 
     public enum WorkScheduleFormat
